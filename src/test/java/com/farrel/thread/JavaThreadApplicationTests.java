d003e2c7b54681f03c44d6d69c5b7fd498d66e2b
Farrel Putra <frl25ap@gmail.com>
2023-10-16 03:51:17 +0700

    Initial commit

---
+++ src/test/java/com/farrel/thread/JavaThreadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farrel.thread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaThreadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
