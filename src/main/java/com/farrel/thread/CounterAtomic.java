f18ee4a0453b46e2122ecee6ab90d908cf2d585b
Farrel Putra <frl25ap@gmail.com>
2024-03-24 13:15:42 +0700

    Add the implementation of Atomic

---
+++ src/main/java/com/farrel/thread/CounterAtomic.java
@@ -0,0 +1,15 @@
+package com.farrel.thread;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class CounterAtomic {
+    private AtomicLong value = new AtomicLong(0L);
+
+    public void increment() {
+        value.incrementAndGet();
+    }
+
+    public Long getValue() {
+        return value.get();
+    }
+}
---
+++ src/test/java/com/farrel/thread/AtomicTest.java
@@ -0,0 +1,31 @@
+package com.farrel.thread;
+
+import org.junit.jupiter.api.Test;
+
+public class AtomicTest {
+
+    @Test
+    void counter() throws InterruptedException {
+        CounterAtomic counter = new CounterAtomic();
+
+        Runnable runnable = () -> {
+            for (int i = 0; i < 1_000_000; i++) {
+                counter.increment();
+            }
+        };
+
+        Thread thread1 = new Thread(runnable);
+        Thread thread2 = new Thread(runnable);
+        Thread thread3 = new Thread(runnable);
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+
+        System.out.println(counter.getValue());
+    }
+}
